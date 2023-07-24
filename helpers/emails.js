93c36a2888a455c8ab1fe2c6dba9593ff6496db5
gacm952 <personalaccounts@gacm952.com>
2023-07-24 12:06:37 -0500

    api mail update

--- helpers/emails.js
+++ helpers/emails.js
@@ -22,3 +22,47 @@ client.messages.create(process.env.EMAIL_HOST, messageData)
  .catch((err) => {
    console.error(err);
  })};
+
+ export const emailForgotPassword = async (Userdata) => {
+
+  const { email } = Userdata;
+
+const mailgun = new Mailgun(formData);
+const client = mailgun.client({username: 'api', key: process.env.EMAIL_PASS});
+
+const messageData = {
+  from: 'Notificaciones CEDYM <contacto@cedym.co>',
+  to: email,
+  subject: 'Confirma tu Cuenta',
+  text: 'TESTEO'
+};
+
+client.messages.create(process.env.EMAIL_HOST, messageData)
+ .then((res) => {
+   console.log(res);
+ })
+ .catch((err) => {
+   console.error(err);
+ })};
+
+ export const emailCreatePassword = async (Userdata) => {
+
+  const { email } = Userdata;
+
+const mailgun = new Mailgun(formData);
+const client = mailgun.client({username: 'api', key: process.env.EMAIL_PASS});
+
+const messageData = {
+  from: 'Notificaciones CEDYM <contacto@cedym.co>',
+  to: email,
+  subject: 'Confirma tu Cuenta',
+  text: 'TESTEO'
+};
+
+client.messages.create(process.env.EMAIL_HOST, messageData)
+ .then((res) => {
+   console.log(res);
+ })
+ .catch((err) => {
+   console.error(err);
+ })};
