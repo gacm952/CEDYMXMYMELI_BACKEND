f3d95d2a7348f2ab40ed50fc9e935304503ef9ee
gacm952 <personalaccounts@gacm952.com>
2023-07-24 16:34:42 -0500

    update mails

--- helpers/emails.js
+++ helpers/emails.js
@@ -15,7 +15,7 @@ const messageData = {
   from: 'Notificaciones CEDYM <notreply@cedym.co>',
   to: email,
   subject: 'Confirma tu Cuenta',
-  text: '¡Hola, ' + name + '! ingresa en este enlace para confirmar tu cuenta ' + process.env.FRONTEND_URL+ '/ConfirmAccount/' + token + '',
+  text: '¡Hola, ' + name + '! ingresa en este enlace para confirmar tu cuenta ' + process.env.FRONTEND_URL + '/ConfirmAccount/' + token + '',
 };
 
 client.messages.create(process.env.EMAIL_HOST, messageData)
@@ -28,16 +28,16 @@ client.messages.create(process.env.EMAIL_HOST, messageData)
 
  export const emailForgotPassword = async (Userdata) => {
 
-  const { email } = Userdata;
+  const{ email, name, token } = Userdata;
 
 const mailgun = new Mailgun(formData);
 const client = mailgun.client({username: 'api', key: process.env.EMAIL_PASS});
 
 const messageData = {
-  from: 'Notificaciones CEDYM <notreply@cedym.co>',
+  from: 'CEDYM<notreply@cedym.co>',
   to: email,
-  subject: 'Confirma tu Cuenta',
-  text: '¡Hola, ' + name + '! ingresa en este enlace para confirmar tu cuenta ' + process.env.FRONTEND_URL+ '/ConfirmAccount/' + token + '',
+  subject: 'Reestablece tu Contraseña',
+  text: '¡Hola, ' + name + '! Para reestablecer tu contraseña ingresa a el siguiente enlace ' + process.env.FRONTEND_URL + '/ConfirmAccount/' + token + '',
 };
 
 client.messages.create(process.env.EMAIL_HOST, messageData)
@@ -50,16 +50,16 @@ client.messages.create(process.env.EMAIL_HOST, messageData)
 
  export const emailCreatePassword = async (Userdata) => {
 
-  const { email } = Userdata;
+  const{ email, name, token } = Userdata;
 
 const mailgun = new Mailgun(formData);
 const client = mailgun.client({username: 'api', key: process.env.EMAIL_PASS});
 
 const messageData = {
-  from: 'Notificaciones CEDYM <notreply@cedym.co>',
+  from: 'CEDYM<notreply@cedym.co>',
   to: email,
-  subject: 'Confirma tu Cuenta',
-  text: 'TESTEO'
+  subject: 'Crea una Contraseña',
+  text: '¡Hola, ' + name + '! haz sido registrado en nuestro sistema, para crear una contraseña y autogestionar tus citas ingresa en el siguiente enlace ' + process.env.FRONTEND_URL + '/ConfirmAccount/' + token + '',
 };
 
 client.messages.create(process.env.EMAIL_HOST, messageData)
