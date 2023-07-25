f04b60fc64365e2752dbeb823b54228d99b677a6
gacm952 <personalaccounts@gacm952.com>
2023-07-25 12:25:05 -0500

    update cors

--- index.js
+++ index.js
@@ -14,21 +14,11 @@ conectarDB();
 
 // Configurar CORS de manera más simple
 
-const whitelist = [process.env.FRONTEND_URL];
-
 const corsOptions = {
-    origin: function(origin, callback) {
-        if (whitelist.includes(origin)) {
-            // Puede consultar la API
-            callback(null, true);
-        } else {
-            // No puede consultar la API
-            callback(new Error("Error de Cors"));
-        }
-    }, 
-};
-
-app.use(cors(corsOptions)); 
+    origin: "https://app.cedym.co",
+  };
+  
+app.use(cors(corsOptions));
 
 // Routing
 
